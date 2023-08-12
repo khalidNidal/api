d45a2304af4c3a556ddf47ea62e11a1728792a2c
khalidNidal <137891091+khalidNidal@users.noreply.github.com>
2023-08-12 21:02:30 +0300

    main.js

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-// let request = new XMLHttpRequest()
-// let data;
-// function getdata(){
-// request.open('GET','https://forkify-api.herokuapp.com/api/search?q=pizza')
-// request.send()
-// request.onreadystatechange = function(){
-//     if(request.readyState==4 && request.status==200)
-//     data = JSON.parse(request.response).recipes  // console.log (request.response) // بطبع سترينج ف بدي احولها ل مصفوفة
-    
-//     console.log (data)
-// }
-// }
-// getdata()
 
-
-//////////////////////////////////////////////////////////////////////////////// new way by using fetch
 
 let data = [] ;
 var links = document.querySelectorAll('.nav-link');
