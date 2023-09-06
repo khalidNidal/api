0583ed2e4bbc5b59835459fa25e927f8bd4ada48
Khalid Nidal Saidi <137891091+khalidNidal@users.noreply.github.com>
2023-09-06 14:58:55 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+// let request = new XMLHttpRequest()
+// let data;
+// function getdata(){
+// request.open('GET','https://forkify-api.herokuapp.com/api/search?q=pizza')
+// request.send()
+// request.onreadystatechange = function(){
+//     if(request.readyState==4 && request.status==200)
+//     data = JSON.parse(request.response).recipes  // console.log (request.response) // بطبع سترينج ف بدي احولها ل مصفوفة
+    
+//     console.log (data)
+// }
+// }
+// getdata()
 
+
+//////////////////////////////////////////////////////////////////////////////// new way by using fetch
 
 let data = [] ;
 var links = document.querySelectorAll('.nav-link');
@@ -34,7 +49,8 @@ function displaydata(){
              <div class="card p-2">
                   <h2>${data[i].title} </h2>
                   <p>${data[i].publisher} </p>
-                  <img class="w-100" src="${data[i].image_url} " alt=""> 
+                  <img class="w-75" src="${data[i].image_url} " alt=""> 
+                  <a class="btn btn-outline-success text-capitalize mt-2 p-2" href="detailes.html?id=${data[i].recipe_id}">show recipe details</a>
              </div>
         </div>
         
